87ef25f346cf07cc93a19823fc151d9b143d4220
Mateusz Przybylski <mattt.przybylski@gmail.com>
2021-12-04 13:00:32 +0100

    home view and add home app to settings

--- congfig/settings.py
+++ congfig/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # 3rd party
+
+    # locals
+    'home.apps.HomeConfig',
 ]
 
 MIDDLEWARE = [
